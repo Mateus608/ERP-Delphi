eecf348120fc16d471f7d8b2fa55f5148b8bd938
Mateus Oliveira Milane <mateus.milane555@gmail.com>
2023-06-14 21:46:24 -0300

    pt 2.1 - Connection Firebird BD - add Query

--- ERP.dpr
+++ ERP.dpr
@@ -3,7 +3,8 @@ program ERP;
 uses
   Vcl.Forms,
   view.principal in 'src\views\view.principal.pas' {ViewPrincipal},
-  service.conexao in 'src\services\service.conexao.pas' {ServiceConexao: TDataModule};
+  service.conexao in 'src\services\service.conexao.pas' {ServiceConexao: TDataModule},
+  service.cadastro in 'src\services\service.cadastro.pas' {ServiceCadastro: TDataModule};
 
 {$R *.res}
 
@@ -12,5 +13,6 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TViewPrincipal, ViewPrincipal);
   Application.CreateForm(TServiceConexao, ServiceConexao);
+  Application.CreateForm(TServiceCadastro, ServiceCadastro);
   Application.Run;
 end.
