1e83cddf7b0e1b926fd7ec843a74dcb7cf7358d8
Mateus Oliveira Milane <mateus.milane555@gmail.com>
2023-06-15 18:38:23 -0300

    pt 3.1 - Form ViewBase.Lista Creation

--- ERP.dpr
+++ ERP.dpr
@@ -6,7 +6,8 @@ uses
   service.conexao in 'src\services\service.conexao.pas' {ServiceConexao: TDataModule},
   service.cadastro in 'src\services\service.cadastro.pas' {ServiceCadastro: TDataModule},
   provider.constants in 'src\providers\provider.constants.pas',
-  view.base in 'src\views\view.base.pas' {ViewBase};
+  view.base in 'src\views\view.base.pas' {ViewBase},
+  view.base.listas in 'src\views\view.base.listas.pas' {ViewBaseListas};
 
 {$R *.res}
 
@@ -16,5 +17,6 @@ begin
   Application.CreateForm(TViewPrincipal, ViewPrincipal);
   Application.CreateForm(TServiceConexao, ServiceConexao);
   Application.CreateForm(TServiceCadastro, ServiceCadastro);
+  Application.CreateForm(TViewBaseListas, ViewBaseListas);
   Application.Run;
 end.
